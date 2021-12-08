0f24c3a1205bce5f2e46e0b46f3d27e2c8de523b
Nenad Krneta <shonely@gmail.com>
2021-12-08 11:58:53 +0100

    Fix undefined var

--- index.js
+++ index.js
@@ -10,13 +10,14 @@ async function run() {
 
   const octokit = github.getOctokit(token);
   const context = github.context;
+
   const result = await octokit.rest.repos.listPullRequestsAssociatedWithCommit({
     owner: context.repo.owner,
     repo: context.repo.repo,
     commit_sha: sha,
   });
 
-  const pr = prs[0];
+  const pr = result[0];
 
   core.setOutput("head_ref", pr.head.ref);
 
